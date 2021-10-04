2cf33c7122a6758969b56affa7755e456eeb6e68
Lesly <leslyr562@gmail.com>
2021-10-03 22:47:33 -0500

    append current weather to card

--- assets/script.js
+++ assets/script.js
@@ -8,32 +8,49 @@
 //uv index=
 
 function myFunction() {
-    console.log("myFunction")
     var searchCity = document.getElementById('searchCity').value;
 
 
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +
         searchCity + "&appid=0b511ddd7a9e1f781148f73c4e808cf6")
-    .then(response => {
-        console.log("res", response)
-        return response.json()
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data.list[0].wind.speed)
+            var cityDisplay = document.querySelector('#cityName')
+            cityDisplay.textContent = searchCity;
+
+            var todayWeather = data.list[0]
+        
+
+            var currentWeather = document.getElementById('details')
+            var listEl = document.createElement('li')
+            listEl.textContent = "Temp: " + todayWeather.main.temp
+            currentWeather.appendChild(listEl)
+
+            var listEl2 = document.createElement('li')
+            listEl2.textContent = "Wind " + todayWeather.wind.speed + " MPH";
+            currentWeather.appendChild(listEl2);
+
+            var listEl3 = document.createElement('li')
+            listEl3.textContent = "Humidity: " + todayWeather.main.humidity + " %"
+            currentWeather.appendChild(listEl3);
+            
+            
+        
+
     })
-    .then(data => {
-        console.log(data)
-    })
-    console.log("here")
-        var cityDisplay = document.querySelector('#cityName')
-        cityDisplay.textContent = searchCity;
-        console.log(cityDisplay)
+
+    
+    
 
 
 
 
 
     // //list//
-    // var currentWeather = document.getElementById('details')
-    // var listEl = document.createElement('li')
-    // listEl.textContent= "Humidity: " + listContent;
+
 
     // .list[0].main.humidity
 
