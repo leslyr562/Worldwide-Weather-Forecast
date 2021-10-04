193e4b4faa91015896c80e23bb024338b168d2e6
Lesly <leslyr562@gmail.com>
2021-10-03 20:38:59 -0500

    Created colums

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,34 @@
 //uv index=
 
 function myFunction() {
+    console.log("myFunction")
     var searchCity = document.getElementById('searchCity').value;
 
 
-fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' + 
-searchCity + "&appid=0b511ddd7a9e1f781148f73c4e808cf6")
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +
+        searchCity + "&appid=0b511ddd7a9e1f781148f73c4e808cf6")
+    .then(response => {
+        console.log("res", response)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+    console.log("here")
+        var cityDisplay = document.querySelector('#cityName')
+        cityDisplay.textContent = searchCity;
+        console.log(cityDisplay)
 
 
-.then(response => response.json())
-.then(data => console.log(data))
 
-var cityDisplay = document.querySelector('#cityName')
-cityDisplay.textContent = searchCity;
+
+
+    // //list//
+    // var currentWeather = document.getElementById('details')
+    // var listEl = document.createElement('li')
+    // listEl.textContent= "Humidity: " + listContent;
+
+    // .list[0].main.humidity
 
 
 }
