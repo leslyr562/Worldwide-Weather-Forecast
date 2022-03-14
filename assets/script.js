95466f940279e9e6ed8b3fb78c82046242eb7651
Lesly <leslyr562@gmail.com>
2022-03-14 13:04:31 -0500

    adding elements

--- assets/script.js
+++ assets/script.js
@@ -1,152 +1,133 @@
 var city = JSON.parse(localStorage.getItem("city")) || [];
 
-
 function myFunction() {
-    var searchCity = document.getElementById('searchCity').value;
+  var searchCity = document.getElementById("searchCity").value;
 
+  
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        searchCity +
+        "&appid=0b511ddd7a9e1f781148f73c4e808cf6&units=imperial"
+    )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data", data);
+      var cityDisplay = document.querySelector("#cityName");
+      var getDates = moment().format("(MM/D/YYYY)");
+      cityDisplay.textContent = searchCity + " " + getDates;
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +
-        searchCity + "&appid=0b511ddd7a9e1f781148f73c4e808cf6&units=imperial")
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            console.log('data', data)
-            var cityDisplay = document.querySelector('#cityName')
-            var getDates = moment().format("(MM/D/YYYY)")
-            cityDisplay.textContent = searchCity + ' ' + getDates ;
+      var todayWeather = data.list[0];
 
-            
+      var currentWeather = document.getElementById("details");
+      currentWeather.innerHTML = "";
 
-            var todayWeather = data.list[0]
+      var listEl = document.createElement("li");
+      listEl.textContent = "Temp: " + todayWeather.main.temp + "°F";
+      currentWeather.appendChild(listEl);
 
-            var currentWeather = document.getElementById('details');
-            currentWeather.innerHTML = "";
+      var listEl2 = document.createElement("li");
+      listEl2.textContent = "Wind " + todayWeather.wind.speed + " MPH";
+      currentWeather.appendChild(listEl2);
 
-            var listEl = document.createElement('li')
-            listEl.textContent = "Temp: " + todayWeather.main.temp
-            currentWeather.appendChild(listEl)
+      var listEl3 = document.createElement("li");
+      listEl3.textContent = "Humidity: " + todayWeather.main.humidity + " %";
+      currentWeather.appendChild(listEl3);
 
-            var listEl2 = document.createElement('li')
-            listEl2.textContent = "Wind " + todayWeather.wind.speed + " MPH";
-            currentWeather.appendChild(listEl2);
+      //card group//
+      ///one///
+      var day1 = data.list[3];
+      var card1 = document.getElementById("detail1");
+      card1.innerHTML = "";
+      var listEl1 = document.createElement("li");
+      listEl.textContent = moment().add(1, 'days').format("(MM/D/YYYY)");
+      card1.appendChild(listEl);
 
-            var listEl3 = document.createElement('li')
-            listEl3.textContent = "Humidity: " + todayWeather.main.humidity + " %"
-            currentWeather.appendChild(listEl3);
+      var listEl = document.createElement("li");
+      listEl.textContent = "Temp: " + day1.main.temp + "°F";
+      card1.appendChild(listEl);
 
-        
-            
+      var listEl2 = document.createElement("li");
+      listEl2.textContent = "Wind " + day1.wind.speed + " MPH";
+      card1.appendChild(listEl2);
 
+      var listEl3 = document.createElement("li");
+      listEl3.textContent = "Humidity: " + day1.main.humidity + " %";
+      card1.appendChild(listEl3);
 
-            //card group//
-            ///one///
-            var card1 = document.getElementById('detail1')
-            card1.innerHTML = "";
-            var listEl = document.createElement('li')
-            listEl.textContent = "Temp: " + todayWeather.main.temp
-            card1.appendChild(listEl)
+      //two//
+      var day2 = data.list[9];
+      var card2 = document.getElementById("detail2");
+      card2.innerHTML = "";
+      var cardEl = document.createElement("li");
+      cardEl.textContent = "Temp: " + day2.main.temp + "°F";
+      card2.appendChild(cardEl);
 
-            var listEl2 = document.createElement('li')
-            listEl2.textContent = "Wind " + todayWeather.wind.speed + " MPH";
-            card1.appendChild(listEl2);
+      var cardEl2 = document.createElement("li");
+      cardEl2.textContent = "Wind " + day2.wind.speed + " MPH";
+      card2.appendChild(cardEl2);
 
-            var listEl3 = document.createElement('li')
-            listEl3.textContent = "Humidity: " + todayWeather.main.humidity + " %"
-            card1.appendChild(listEl3);
+      var cardEl3 = document.createElement("li");
+      cardEl3.textContent = "Humidity: " + day2.main.humidity + " %";
+      card2.appendChild(cardEl3);
 
-            //two//
-            var day2 = data.list[7]
-            var card2 = document.getElementById('detail2')
-            card2.innerHTML = "";
-            var cardEl = document.createElement('li')
-            cardEl.textContent = "Temp: " + day2.main.temp
-            card2.appendChild(cardEl)
+      //three//
+      var day3 = data.list[17];
+      var card3 = document.getElementById("detail3");
+      card3.innerHTML = "";
+      var detailEl = document.createElement("li");
+      detailEl.textContent = "Temp: " + day3.main.temp + "°F";
+      card3.appendChild(detailEl);
 
-            var cardEl2 = document.createElement('li')
-            cardEl2.textContent = "Wind " + day2.wind.speed + " MPH";
-            card2.appendChild(cardEl2);
+      var detailEl2 = document.createElement("li");
+      detailEl2.textContent = "Wind " + day3.wind.speed + " MPH";
+      card3.appendChild(detailEl2);
 
-            var cardEl3 = document.createElement('li')
-            cardEl3.textContent = "Humidity: " + day2.main.humidity + " %"
-            card2.appendChild(cardEl3);
+      var detailEl3 = document.createElement("li");
+      detailEl3.textContent = "Humidity: " + day3.main.humidity + " %";
+      card3.appendChild(detailEl3);
 
-            //three//
-            var day3 = data.list[15]
-            var card3 = document.getElementById('detail3');
-            card3.innerHTML = "";
-            var detailEl = document.createElement('li')
-            detailEl.textContent = "Temp: " + day3.main.temp
-            card3.appendChild(detailEl)
+      //four//
+      var day4 = data.list[25];
+      var card4 = document.getElementById("detail4");
+      card4.innerHTML = "";
+      var dateEl = document.createElement("li");
+      dateEl.textContent = "Temp: " + day4.main.temp + "°F";
+      card4.appendChild(dateEl);
 
-            var detailEl2 = document.createElement('li')
-            detailEl2.textContent = "Wind " + day3.wind.speed + " MPH";
-            card3.appendChild(detailEl2);
+      var dateEl2 = document.createElement("li");
+      dateEl2.textContent = "Wind " + day4.wind.speed + " MPH";
+      card4.appendChild(dateEl2);
 
-            var detailEl3 = document.createElement('li')
-            detailEl3.textContent = "Humidity: " + day3.main.humidity + " %"
-            card3.appendChild(detailEl3);
+      var dateEl3 = document.createElement("li");
+      dateEl3.textContent = "Humidity: " + day4.main.humidity + " %";
+      card4.appendChild(dateEl3);
 
+      //five//
 
-            //four//
-            var day4 = data.list[24]
-            var card4 = document.getElementById('detail4')
-            card4.innerHTML = "";
-            var dateEl = document.createElement('li')
-            dateEl.textContent = "Temp: " + day4.main.temp
-            card4.appendChild(dateEl)
+      var day5 = data.list[39];
+      var card5 = document.getElementById("detail5");
+      card5.innerHTML = "";
+      var dayEl = document.createElement("li");
+      dayEl.textContent = "Temp: " + day5.main.temp + "°F";
+      card5.appendChild(dayEl);
 
-            var dateEl2 = document.createElement('li')
-            dateEl2.textContent = "Wind " + day4.wind.speed + " MPH";
-            card4.appendChild(dateEl2);
+      var dayEl2 = document.createElement("li");
+      dayEl2.textContent = "Wind " + day5.wind.speed + " MPH";
+      card5.appendChild(dayEl2);
 
-            var dateEl3 = document.createElement('li')
-            dateEl3.textContent = "Humidity: " + day4.main.humidity + " %"
-            card4.appendChild(dateEl3);
+      var dayEl3 = document.createElement("li");
+      dayEl3.textContent = "Humidity: " + day5.main.humidity + " %";
+      card5.appendChild(dayEl3);
 
+      if (city.indexOf(searchCity) === -1) {
+        city.push(searchCity);
+        localStorage.setItem("city", JSON.stringify(city));
+      }
+    });
 
-            //five//
+  // //list//
 
-            var day5 = data.list[39]
-            var card5 = document.getElementById('detail5')
-            card5.innerHTML = "";
-            var dayEl = document.createElement('li')
-            dayEl.textContent = "Temp: " + day5.main.temp
-            card5.appendChild(dayEl)
-
-            var dayEl2 = document.createElement('li')
-            dayEl2.textContent = "Wind " + day5.wind.speed + " MPH";
-            card5.appendChild(dayEl2);
-
-            var dayEl3 = document.createElement('li')
-            dayEl3.textContent = "Humidity: " + day5.main.humidity + " %"
-            card5.appendChild(dayEl3);
-
-
-    
-            
-               if( city.indexOf(searchCity) === -1) {
-                city.push(searchCity)
-                localStorage.setItem('city', JSON.stringify(city));
-               }
-               
-            
-
-
-
-        })
-
-
-
-
-
-
-
-
-    // //list//
-
-
-    // .list[0].main.humidity
-
-
+  // .list[0].main.humidity
 }
